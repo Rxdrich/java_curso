5cec07f061bdf11de628a731463402c118643889
Rxdrich <angelroro520@gmail.com>
2024-01-24 20:00:57 -0300

    commit desde VS

--- Fundamentos/HolaMundo.java
+++ Fundamentos/HolaMundo.java
@@ -2,6 +2,6 @@ package Fundamentos;
 
 public class HolaMundo {
     public static void main(String args[]) {
-         System.out.println("Hola Mundo desde Java");
+         System.out.println("Hola Mundo");
     }
 }
