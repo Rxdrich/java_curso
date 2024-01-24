369f82d3653c6226862f63ae21b5cba01f6b876f
Rxdrich <angelroro520@gmail.com>
2024-01-24 20:18:40 -0300

    8. Tipos Enteros y Tipo String en Java

--- Fundamentos/HolaMundo.java
+++ Fundamentos/HolaMundo.java
@@ -2,7 +2,18 @@ package Fundamentos;
 
 public class HolaMundo {
     public static void main(String args[]) {
+        //Definimos la variable
          int miVarEntera = 10;
          System.out.println(miVarEntera);
+
+         //Modificamos el valor de la variable;
+         miVarEntera = 7;
+         System.out.println(miVarEntera);
+
+         String miVarCadena = "Saludos";
+         System.out.println(miVarCadena);
+         
+         miVarCadena = "Adios";
+         System.out.println(miVarCadena);
     }
 }
