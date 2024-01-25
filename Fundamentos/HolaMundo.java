b733441a233f1b056a68c0038d93a1a1748021d4
Rxdrich <angelroro520@gmail.com>
2024-01-25 20:17:27 -0300

    12. Caracteres Especiales con Java

--- Fundamentos/HolaMundo.java
+++ Fundamentos/HolaMundo.java
@@ -7,16 +7,12 @@ public class HolaMundo {
      * @param args
      */
     public static void main(String args[]) {
-        var usuario = "Angel";
-        var titulo = "Ingeniero";
+        var nombre = "Karla";
 
-        var union = titulo + " " + usuario;
-        System.out.println(union);
-
-        var i = 3;
-        var j = 4;
-        System.out.println(i + j); //se realiza la suma de numeros
-        System.out.println(i + j + " " + usuario); // evaluacion de izquierda a derecha realiza suma
-        System.out.println(usuario + (i + j)); // uso de parentesis modifican la prioridad en la evaluacion
+        System.out.println("Nueva linea: \n" + nombre);
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("Retroceso: \b" + nombre );
+        System.out.println("Comilla simple: \'" + nombre + "'" );
+        System.out.println("Comilla Doble: \"" + nombre + "\" ");
     }
 }
