37080f3a6571bc2d6f1cc5b9f4a6bb7e7da62847
Rxdrich <angelroro520@gmail.com>
2024-01-28 22:33:48 -0300

    13. Clase Scanner en Java

--- Fundamentos/HolaMundo.java
+++ Fundamentos/HolaMundo.java
@@ -2,17 +2,19 @@ package Fundamentos;
 
 import javax.print.attribute.SupportedValuesAttribute;
 
+import java.util.Scanner;
+
 public class HolaMundo {
     /**
      * @param args
      */
     public static void main(String args[]) {
-        var nombre = "Karla";
-
-        System.out.println("Nueva linea: \n" + nombre);
-        System.out.println("Tabulador: \t" + nombre);
-        System.out.println("Retroceso: \b" + nombre );
-        System.out.println("Comilla simple: \'" + nombre + "'" );
-        System.out.println("Comilla Doble: \"" + nombre + "\" ");
+        System.out.println("Escribe tu nombre:");
+        Scanner consola = new Scanner(System.in);
+        var usuario = consola.nextLine(); // lee una linea completa de la consola
+        System.out.println(usuario);
+        System.out.println("Escribe el titulo");
+        var titulo = consola.nextLine();
+        System.out.println("Resultado: " + titulo + " " + usuario);
     }
 }
